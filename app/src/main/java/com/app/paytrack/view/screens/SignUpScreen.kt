c269b3459c1f1e31e6f4c58cbc6d803b0ac86421
mohamadjamous <mohamad.e.jamous@gmail.com>
2025-03-27 12:15:46 +0200

    feat: add sign up screen ui

---
+++ app/src/main/java/com/app/paytrack/view/screens/SignUpScreen.kt
@@ -0,0 +1,234 @@
+package com.app.paytrack.view.screens
+
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.app.paytrack.R
+import com.app.paytrack.view.components.CustomButton
+import com.app.paytrack.view.components.CustomTextField
+import com.app.paytrack.view.sign_in.SignInState
+import java.util.Locale
+
+@Composable
+fun SignUpScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    state: SignInState,
+    onGoogleSignInClick: () -> Unit
+) {
+
+    val context = LocalContext.current
+
+    LaunchedEffect(state.signInError) {
+
+        state.signInError?.let { error ->
+            Toast.makeText(
+                context,
+                error,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    var name by remember {
+        mutableStateOf("")
+    }
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    var conPassword by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 40.dp, start = 20.dp, end = 20.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+
+
+        Text(
+            text = stringResource(id = R.string.sign_up),
+            fontWeight = FontWeight.Bold,
+            fontSize = 35.sp,
+            color = colorResource(id = R.color.dark_green)
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            CustomTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = email,
+                hint = stringResource(id = R.string.name),
+                leadingIcon = Icons.Outlined.Person
+            ) {
+                email = it
+            }
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            CustomTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = password,
+                hint = stringResource(id = R.string.email),
+                leadingIcon = Icons.Outlined.Email,
+                passwordVisible = false
+            ) {
+                password = it
+            }
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            CustomTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = password,
+                hint = stringResource(id = R.string.password),
+                leadingIcon = Icons.Outlined.Lock,
+                passwordVisible = true
+            ) {
+                password = it
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+
+            CustomTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = password,
+                hint = stringResource(id = R.string.confirm_password),
+                leadingIcon = Icons.Outlined.Lock,
+                passwordVisible = true
+            ) {
+                password = it
+            }
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        CustomButton(
+            text = stringResource(id = R.string.sign_up).uppercase(Locale.ROOT)
+        ) {
+
+        }
+
+        OrDivider(
+            modifier = Modifier.padding(top = 35.dp)
+        )
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(
+                modifier = Modifier
+                    .padding(top = 10.dp),
+                text = stringResource(id = R.string.sign_in_with),
+                color = colorResource(id = R.color.gray),
+                fontWeight = FontWeight.Bold,
+                fontSize = 17.sp
+            )
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            IconButton(
+                content = {
+                    Icon(
+                        modifier = Modifier.size(30.dp),
+                        painter = painterResource(id = R.drawable.google),
+                        contentDescription = null,
+                        tint = Color.Unspecified
+                    )
+                },
+                onClick = {
+                    onGoogleSignInClick()
+                }
+            )
+
+            Row(
+                modifier = Modifier.padding(top = 20.dp)
+            ) {
+                Text(
+                    modifier = Modifier
+                        .padding(top = 10.dp),
+                    text = stringResource(id = R.string.already_have_account),
+                    color = colorResource(id = R.color.gray),
+                    fontSize = 17.sp
+                )
+
+                Text(
+                    modifier = Modifier
+                        .padding(top = 10.dp, start = 5.dp)
+                        .clickable {
+                            navController.popBackStack()
+                        },
+                    text = stringResource(id = R.string.sign_in),
+                    color = colorResource(id = R.color.gray),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 17.sp
+                )
+            }
+
+        }
+
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignUpScreenPreview(modifier: Modifier = Modifier) {
+    SignUpScreen(
+        navController = rememberNavController(),
+        state = SignInState(isSignInSuccessful = false, signInError = null),
+        onGoogleSignInClick = {})
+}
